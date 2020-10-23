147842463f2fef3beeacf5a29b8dee605a42ec65
ambrwlsn <arw89@uclive.ac.nz>
2020-10-23 22:03:16 +0200

    Add tags

--- .eleventy.js
+++ .eleventy.js
@@ -56,6 +56,37 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setLibrary('md', markdownIt(MARKDOWN_OPTIONS));
 
+  // https://www.11ty.dev/docs/quicktips/tag-pages/
+  eleventyConfig.addCollection('tagList', function(collection) {
+    let tagSet = new Set();
+    collection.getAll().forEach(function(item) {
+      if ('tags' in item.data) {
+        let tags = item.data.tags;
+
+        tags = tags.filter(function(item) {
+          switch (item) {
+            // this list should match the `filter` list in tags.njk
+            case 'all':
+            case 'post':
+            case 'tagList':
+            case 'bookReview':
+            case 'wilt':
+              return false;
+          }
+
+          return true;
+        });
+
+        for (const tag of tags) {
+          tagSet.add(tag);
+        }
+      }
+    });
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...tagSet];
+  });
+
   return {
     dir: {
       input: 'src',
--- src/tags/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Tags
-eleventyExcludeFromCollections: true
----
-
-## Tags
