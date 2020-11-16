02e224dbdd4d5e3a68a1f46447ba60e6b5036a3c
ambrwlsn <arw89@uclive.ac.nz>
2020-11-16 19:47:30 +0100

    Add social media image

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function(eleventyConfig, options) {
   eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
   eleventyConfig.addPassthroughCopy('./src/browserconfig.xml');
   eleventyConfig.addPassthroughCopy('./src/mstile-150x150.png');
+  eleventyConfig.addPassthroughCopy('./src/amber.png');
   eleventyConfig.addPassthroughCopy('./src/about/img/**');
   eleventyConfig.addPassthroughCopy('./src/projects/img/**');
   eleventyConfig.addPassthroughCopy('./src/read/img/**');
