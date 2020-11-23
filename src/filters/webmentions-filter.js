b682f00d444240f3d82c87e386195f2f0c3d4f89
ambrwlsn <arw89@uclive.ac.nz>
2020-11-23 11:43:14 +0100

    Allow other response types on webmentions

--- src/filters/webmentions-filter.js
+++ src/filters/webmentions-filter.js
@@ -4,7 +4,13 @@ module.exports = function(webmentions, url) {
   // define which types of webmentions should be included per URL.
   // possible values listed here:
   // https://github.com/aaronpk/webmention.io#find-links-of-a-specific-type-to-a-specific-page
-  const allowedTypes = ['mention-of', 'in-reply-to'];
+  const allowedTypes = [
+    'mention-of',
+    'in-reply-to',
+    'repost-of',
+    'like-of',
+    'bookmark-of',
+  ];
 
   // define which HTML tags you want to allow in the webmention body content
   // https://github.com/apostrophecms/sanitize-html#what-are-the-default-options
