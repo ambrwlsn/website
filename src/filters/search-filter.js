7f1223c36fe4333741ca962a668fed46ca4d7b5d
ambrwlsn <arw89@uclive.ac.nz>
2020-12-17 22:09:45 +0100

    Improve search filters

--- src/filters/search-filter.js
+++ src/filters/search-filter.js
@@ -6,6 +6,7 @@ module.exports = function(collection) {
     this.addField('title');
     this.addField('tags');
     this.addField('date');
+    this.addField('keywords');
     this.setRef('id');
   });
 
@@ -13,6 +14,7 @@ module.exports = function(collection) {
     index.addDoc({
       title: page.data.title,
       tags: page.data.tags,
+      keywords: page.data.keywords,
       date: format(new Date(page.data.date), 'MMM do yyyy'),
       id: page.url,
     });
