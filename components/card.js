d6b900d7886d736c2f10c6c7482f8109556c48bc
ambrwlsn <arw89@uclive.ac.nz>
2021-09-15 09:40:34 +0200

    Improve accessible names

--- components/card.js
+++ components/card.js
@@ -4,8 +4,8 @@ module.exports = function(text, purpose) {
   }
 
   if (purpose === 'why') {
-    return `<div class="card why-card">${text}</div>`;
+    return `<div class="card why-card"><span class="card-icon" aria-hidden="true"></span>${text}</div>`;
   }
 
-  return `<div class="card">${text}</div>`;
+  return `<div class="card"><span class="card-icon" aria-hidden="true"></span>${text}</div>`;
 };
