2d13b53acab221a6e1e9acdccb3447917814f592
ambrwlsn <arw89@uclive.ac.nz>
2021-09-27 21:24:31 +0200

    Add card variant

--- components/card.js
+++ components/card.js
@@ -3,6 +3,10 @@ module.exports = function(text, purpose) {
     return null;
   }
 
+  if (purpose === 'why-bullet-list') {
+    return `<div class="card why-card why-card__bullet-list"><span class="card-icon" aria-hidden="true"></span>${text}</div>`;
+  }
+
   if (purpose === 'why') {
     return `<div class="card why-card"><span class="card-icon" aria-hidden="true"></span>${text}</div>`;
   }
